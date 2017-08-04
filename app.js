6921867cf9ee2b97f8b41845ff7965a7912171ca
Chad Stephenson <chad@coastald.com>
2017-08-04 14:53:23 -0500

    new

---
+++ app.js
@@ -0,0 +1,83 @@
+/*--- Step 1 - Defining global variables ---*/
+
+var questionsArray = [
+    //Question 1
+    {
+        questionText: 'When did the predessor to the modern guitar first appear?',
+        questionChoices: ['The 11th Century', 'The 16th Century', 'The 19th Century'],
+        questionCorrectChoice: 1,
+        correctDetails: 'The baroque guitar, the predessor most closely related to the modern guitar appeared in the middle of the 16th centrury.'
+    },
+
+    //Question 2
+    {
+        questionText: 'What is the smallest guitar in the world?',
+        questionChoices: ['Nanoguitar', 'Erlwine Laser', 'PalmGuitar'],
+        questionCorrectChoice: 0,
+        correctDetails: 'The smallest guitar is 10 micrometers long and constructed out of silicon in a Cornell University labratory.'
+    },
+
+    //Question 3
+    {
+        questionText: 'How long is the world\'s largest playable guitar in the world?',
+        questionChoices: ['4 feet', '26 feet', "43+ feet"],
+        questionCorrectChoice: 2,
+        correctDetails: 'The world\' s largest playable guitar weighs over 2255 pounds and is 43.5 feet long!'
+    },
+
+    //Question 4
+    {
+        questionText: 'What is the highest recorded price paid for a guitar?',
+        questionChoices: ['$900,000.00', '$1,560,000.00', '$2,700,00.00'],
+        questionCorrectChoice: 2,
+        correctDetails: 'The guitar that fetched the highest price was auctioned off to raise money for the 2004 Asia Tsunami relief charity.'
+    },
+
+    //Question 5
+    {
+        questionText: 'What year was the electric guitar introduced?',
+        questionChoices: ['1897', '1925', '1931'],
+        questionCorrectChoice: 2,
+        correctDetails: 'The first electrically amplified guitar was designed in 1931 by George Beauchamp, the general manager of the National Guitar Corporation, with Paul Barth, who was vice president.'
+    },
+
+    //Question 6
+    {
+        questionText: 'What is the highest string count on a playable guitar?',
+        questionChoices: ['12', '42', '88'],
+        questionCorrectChoice: 1,
+        correctDetails: 'Pikasso is a custom instrumentwith 42 strings that was created for Pat Metheny in 1984'
+    },
+
+    //Question 7
+    {
+        questionText: 'How large is the biggest guitar collection?',
+        questionChoices: ['672', '1400', '3000'],
+        questionCorrectChoice: 2,
+        correctDetails: 'Keith Richards of the Rolling Stones owns more than 3000 guitars.'
+    },
+
+    //Question 8
+    {
+        questionText: 'What is the longest known guitar solo ever played?',
+        questionChoices: ['16+ hours', '24+ hours', "43+ hours"],
+        questionCorrectChoice: 1,
+        correctDetails: 'David DiDonato shredded for a cartilage-melting 24 hours and 55 minutes on stage in 2012.'
+    },
+
+    //Question 9
+    {
+        questionText: 'What is the longest known guitar playing performance?',
+        questionChoices: ['24+ hours', '43+ hours', '114+ hours'],
+        questionCorrectChoice: 2,
+        correctDetails: 'In 2011, David Browne played for 114 hours and six minutes, playing 1374 songs back to back.'
+    },
+
+    //Question 10
+    {
+        questionText: 'What is the record for most guitar strings changed in an hour?',
+        questionChoices: ['62', '156', '183'],
+        questionCorrectChoice: 2,
+        correctDetails: 'Glenn Haworth changed 183 individual strings in just 60 minutes. '
+    }
+];
